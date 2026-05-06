4244f7c56b884fded2b4dde5dde9550f4fdc6abc
Devyansh <Devyanshraj1511@gmail.com>
2026-05-06 12:32:50 -0400

    v01 fixed

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from fastapi import FastAPI,HttpResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 import random
 import string
 from models import *
 
 app = FastAPI()
 urls_db = {}
-counter = 0
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
 @app.post("/shorten")
 def shorten_url(url: URL):
@@ -23,8 +19,7 @@ def shorten_url(url: URL):
         created_at=datetime.datetime.now(),
         clicks=0
     )
-    urls_db[counter] = url_item
-    counter += 1
+    urls_db[short_url] = url_item
     return url_item
 
 @app.get("/s/{short_url}")
@@ -32,8 +27,8 @@ def redirect_url(short_url: str):
     url_item = urls_db.get(short_url)
     if url_item:
         url_item.clicks += 1
-        return HttpResponse(url_item.original_url)
-    return HttpResponse("URL not found", status_code=404)
+        return RedirectResponse(url_item.original_url)
+    raise HTTPException("URL not found", status_code=404)
 
 @app.get("/urls")
 def get_urls():
@@ -44,12 +39,12 @@ def get_url(short_url: str):
     url_item = urls_db.get(short_url)
     if url_item:
         return url_item
-    return HttpResponse("URL not found", status_code=404)
+    raise HTTPException("URL not found", status_code=404)
 
 @app.delete("/urls/{short_url}")
 def delete_url(short_url: str):
     if short_url in urls_db:
         urls_db.pop(short_url)
-        return HttpResponse("URL deleted")
-    return HttpResponse("URL not found", status_code=404)
+        raise HTTPException("URL deleted")
+    raise HTTPException("URL not found", status_code=404)
 
